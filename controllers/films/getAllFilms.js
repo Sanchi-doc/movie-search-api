582591a895d24e19b57f600e0cf3d421a7eda848
Oleksandr <babichenkoaleksandr@gmail.com>
2026-04-15 11:33:37 +0300

    add total pages calculation to getAllFilms response

--- controllers/films/getAllFilms.js
+++ controllers/films/getAllFilms.js
@@ -5,12 +5,16 @@ const getAllFilms = async (req, res) => {
     const {page = 1, limit = 20} = req.query
     const skip = (page-1) * limit
     const results = await Film.find({owner: _id}, "-createdAt -updatedAt", {skip, limit: +limit})
+    const totalFilms = await Film.find({owner: _id})
+    const total_pages = Math.ceil(totalFilms.length/limit)
+    console.log('total',total_pages);
     
     
     res.status(200).json({
         response: 'success',
         status: 200,
-        results
+        results,
+        total_pages
     })
 } 
 module.exports = getAllFilms
